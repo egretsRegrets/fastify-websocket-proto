bbe181ef2c27f5a1032a13878765c5e62513e40c
Sam Gardner <sam.galen.gardner@gmail.com>
2025-06-16 16:11:09 -0400

    quick fix, actually handling the message for the user connected via homepage

--- public/index.js
+++ public/index.js
@@ -2,6 +2,6 @@ const _ws = new WebSocket(
   `ws://${window.location.host}/chat?username=long-legs`
 );
 _ws.onmessage = (message) => {
-  message = JSON.parse(message.data);
-  appendMessage(message);
+  const msg = JSON.parse(message.data);
+  console.log(msg);
 };
