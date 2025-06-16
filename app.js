edbcb6be6634ba4b5ea397eda675f0944ad5b700
Sam Gardner <sam.galen.gardner@gmail.com>
2025-06-16 13:53:22 -0400

    echo message on websocket endpoint

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 import Fastify from "fastify";
+import FastifyWebsocket from "@fastify/websocket";
 
 const server = Fastify({
   logger: true,
 });
 
-server.get("/", function rootHandler(request, reply) {
+await server.register(FastifyWebsocket);
+
+server.get("/", function handleRoot(request, reply) {
   reply.send({ hello: "world" });
 });
 
-server.listen({ port: 3000 }, function (err, address) {
+server.get("/ws", { websocket: true }, function handleWS(socket, req) {
+  socket.on("message", function (message) {
+    socket.send(
+      `Hello from fastify web socket, your message was: "${message}"`
+    );
+  });
+});
+
+await server.listen({ port: 3000 }, function (err, address) {
   if (err) {
     server.log.error(err);
     process.exit(1);
